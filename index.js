44cc06f886f35a965a53430436f6f37935366455
Stephen Schmuhl <schmuhl2@outlook.com>
2018-07-04 22:33:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,11 +5,11 @@ dodger.style.backgroundColor = '#FF69B4'
 document.addEventListener('keydown', function(e) {
   console.log(e.which)
 })
-document.addEventListener('keydown', function(e) {
-  if (e.which === 37) {
-    var leftNumbers = dodger.style.left.replace('px', '')
-    var left = parseInt(leftNumbers, 10)
+function moveDodgerLeft() {
+  var leftNumbers = dodger.style.left.replace('px', '')
+  var left = parseInt(leftNumbers, 10)
  
+  if (left > 0) {
     dodger.style.left = `${left - 1}px`
   }
-})
+}
