1bec427c837bcf774e3f9379c4529a4ce18409af
Stephen Schmuhl <schmuhl2@outlook.com>
2018-07-04 22:32:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,11 @@ dodger.style.backgroundColor = '#FF69B4'
 document.addEventListener('keydown', function(e) {
   console.log(e.which)
 })
+document.addEventListener('keydown', function(e) {
+  if (e.which === 37) {
+    var leftNumbers = dodger.style.left.replace('px', '')
+    var left = parseInt(leftNumbers, 10)
+ 
+    dodger.style.left = `${left - 1}px`
+  }
+})
