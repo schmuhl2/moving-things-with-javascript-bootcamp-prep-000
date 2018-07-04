7b9ea637927167ecee0ad339046fb7397d41a1cb
Stephen Schmuhl <schmuhl2@outlook.com>
2018-07-04 22:32:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,6 @@ const app = "I don't do much."
 var dodger = document.getElementById('dodger')
 dodger.style.backgroundColor = "#000000"
 dodger.style.backgroundColor = '#FF69B4'
+document.addEventListener('keydown', function(e) {
+  console.log(e.which)
+})
